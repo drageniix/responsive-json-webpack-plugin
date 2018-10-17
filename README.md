89cfee9b1fef190eba16abd3ec07f8476522dcb4
Drageniix <drageniix@gmail.com>
2018-10-17 09:33:25 -0500

    Initial publication, needs new tests

---
+++ README.md
@@ -0,0 +1,266 @@
+# @drageniix/responsive-json-webpack-plugin
+
+[![npm bundle size (minified)](https://img.shields.io/bundlephobia/min/react.svg)](https://github.com/drageniix/responsive-json-webpack-plugin)
+
+Set resizing instructions in JSON, can also output combined JSON with responsive image attributes (picture, sources, srcset, sizes, media) included where desired.
+
+## Basic Image Resize Usage
+
+```json
+[
+    {
+        "files": [
+            {
+                "src": "sample.png",
+                "size": 16
+            },
+            {
+                "src": "sample.jpg",
+                "size": 32
+            }
+        ]
+    }
+]
+```
+
+Will create an optimized and compressed 16px width "sample.png" and 32px width "sample.jpg" in the output folder.
+
+```json
+[
+    {
+        "files": [
+            {
+                "src": "sample-2.png",
+                "dest" : "helloworld",
+                "size": 16
+            }
+        ],
+        "alternates": [
+            {
+                "size": 36,
+                "dest": "[name]-huge"
+            },
+            {
+                "size": 8,
+                "dest": "[name]-[size]"
+            }
+        ]
+    }
+]
+```
+
+Will create a 36px width "helloworld-huge.png" and an 8px width "helloworld-8.png" in output folder.
+
+## JSON Injection usage
+
+### In "templates/index/data/sample.json"
+```json
+{
+    "text" : "This is sample text.",
+    "section" : {
+        "title" : "Sample Section Title"
+    }
+}
+```
+
+### In "templates/index/data/other.json"
+```json
+{
+    "text" : "This is other sample text.",
+}
+```
+
+### In "templates/index/images/sample.json"
+```json
+[
+    {
+        "path" : "image",
+        "alt" : "standard sample image",
+        "files" : [
+            {
+                "src" : "sample-3.png",
+                "size" : 16
+            }
+        ] 
+    },
+    {
+        "path" : "section.image",
+        "files" : [
+            {
+                "src" : "sample-3.png",
+                "dest" : "section",
+                "size" : 24
+            }
+        ] 
+    }
+]
+```
+
+Will output a 16px width "sample-3.png" and a 24px with "section.png" along with a file at **index.json**:
+
+```json
+{
+    "sample" : {
+        "text" : "This is sample text.",
+        "image" : {
+            "src" : "sample-3.png",
+            "alt" :  "standard sample image",
+            "size" : 16          
+        },
+        "section" : {
+            "title" : "Sample Section Title",
+            "image" : {
+                "src" : "section.png",
+                "size" : 24          
+            }
+        }
+    },
+    "other" : {
+        "text" : "This is other sample text.",
+    }
+}
+```
+
+## Advanced JSON Injection
+
+You can output an array at the path by giving multiple files.
+```json
+[
+    {
+        "path" : "multiple",
+        "files" : [
+            {
+                "src" : "sample-2.png",
+                "size" : 16
+            },
+            {
+                "src" : "sample-3.png",
+                "size" : 16
+            }
+        ] 
+    }
+]
+```
+
+You can also inject an image at each item in an exisiting an array by using this syntax. The "[]" will be replaced by the index of the item in the set. The index does not need to match the exisiting array in length, but does need a valid path.
+
+Array set dest can use "[name]" "[size]" and "[index]", while standard usage can use "[name]" and "[size]".
+
+```json
+{
+    "path" : "array.[].image",
+    "set" : [
+        {
+            "alt" : "array item",
+            "files" : [
+                {
+                    "src" : "sample-arr.png",
+                    "size" : 16,
+                    "dest" : "array-image-[index]"
+                }
+            ]
+        }
+    ]
+}
+```
+This will output: 
+```json
+{
+    "array" : [
+        {
+            "text" : "existing text in data json",
+            "image" : {
+                "alt" : "array item 1",
+                "src" : "arr-image-1.png",
+                "size" : 16          
+            }
+        }
+    ]
+}
+```
+
+## Responsive Usage
+
+Here is a full responsive image example that utilizes both resolution switching and art direction! If your "files" image has a "dest", that will be the basis of "[name]" in the "imageTemplate" "img". Your "files" images are always copied, even if never explicitly used, to serve as a backup in case a browser doesn't support the "picture" element or the like.
+
+Keep in mind that most fields can be safely omitted, but are recommended if you are using this with the included ResponsiveImage.jsx file.
+
+```json
+[
+    {
+        "path" : "responsive",
+        "alt" : "Sample ALT",
+        "files" : [
+            {
+                "src" : "sample-8.png",
+                "size" : 16
+            }
+        ],
+        "imageTemplate": {
+            "img": {
+                "sizes": "(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px",
+                "srcset": [
+                    {
+                        "dest": "[name]x8",
+                        "size": 8
+                    }
+                ]
+            },
+            "sources" : [
+                {
+                    "media" : "(max-width: 37.5em)",
+                    "sizes" : "20vw",
+                    "srcset" : [
+                        {
+                            "src" : "sample-9.png",
+                            "dest" :  "potato",
+                            "size" : 16
+                        },
+                        {
+                            "src": "sample-10.png",
+                            "dest" :  "[name]-x[size]",
+                            "size" : 16
+                        }
+                    ]
+                }
+            ]
+        }
+    }
+]
+```
+
+Which will output:
+```json
+{
+    "src" : "sample-8.png",
+    "alt" : "Sample ALT",
+    "size" : 16,
+    "img" : {
+        "src" : "sample-8.png",
+        "sizes" : "(max-width: 56.25em) 20vw, (max-width: 37.5em) 30vw, 300px",
+        "alt" : "Sample ALT",
+        "srcset" : [
+            {
+                "src" : "sample-8x8",
+                "size" : 8
+            }
+        ]
+    },
+    "sources" : [
+        {
+            "media" : "(max-width: 37.5em)",
+            "sizes" : "20vw",
+            "srcset" : [
+                {
+                    "src" : "potato.png",
+                    "size" : 16
+                },
+                {
+                    "src" : "sample-10-x16",
+                    "size" : 16
+                }
+            ]
+        }
+    ],
+}
+```
---
+++ index.js
@@ -0,0 +1,311 @@
+const fs = require("fs-extra")
+const sharp = require("sharp")
+
+module.exports = class ResponsiveJSONWebpackPlugin {
+    constructor(
+        { 
+            dataPath = "data", 
+            imagePath = "images", 
+            sourceTemplates = "src/assets/templates", 
+            sourceImages = "src/assets/images", 
+            outputFolder = "assets"
+        } = {}) {
+        
+        this.folders = {}
+        this.files = {}
+        this.slashRegex = new RegExp("/", "g")
+        this.stripRegex = /^\/+|^\.\/+|\/+$/g
+        this.dirs = {
+            dataPath: dataPath.replace(this.stripRegex, ""),
+            imagePath: imagePath.replace(this.stripRegex, ""),
+            sourceTemplates: sourceTemplates.replace(this.stripRegex, ""),
+            sourceImages: sourceImages.replace(this.stripRegex, ""),
+            outputFolder: outputFolder.replace(this.stripRegex, "")
+        }
+    }
+
+    run(compilation){
+        const dependencies = this.getChangedDependencies(compilation)
+
+        this.processedFileNames = []
+        this.assets = compilation.assets
+        this.folders = dependencies.folders
+        this.files = dependencies.files
+
+        return this.processDataFolders(
+            dependencies.changedFolders,
+        ).then(() => this.processRawFiles(
+            dependencies.changedPureFiles,
+        ))
+    }
+
+    apply(compiler) {
+        const self = this
+        compiler.hooks.emit.tapPromise("ResponsiveJSONPlugin", this.run.bind(self))
+    }
+
+    saveJSON(folder, jsonMap) {
+        const stringData = JSON.stringify(Object.assign({}, ...jsonMap))
+        this.assets[`./${this.dirs.outputFolder}/${this.dirs.dataPath}/${folder}.json`] = {
+            source: () => Buffer.from(stringData),
+            size: () => stringData.length,
+        }
+    }
+
+    savePicture(sourceFilePath, { src, size }) {
+        if (!this.processedFileNames.includes(src)) {
+            this.processedFileNames.push(src)
+            return sharp(sourceFilePath).resize(size).toBuffer({ resolveWithObject: true })
+                .then(({ data, info }) => { //todo: webp fallback
+                    this.assets[`./${src}`] = {
+                        source: () => data,
+                        size: () => info.size,
+                    }
+                })
+        } else return Promise.resolve()
+    }
+
+    processRawFiles(dataFiles) {
+        return Promise.all(dataFiles.map(file =>
+            fs.readJSON(file).then(json =>
+                Promise.all(json.map(({ files, alternates }) =>
+                    this.processRawItem(files, alternates))))
+        ))
+    }
+
+    processDataFolders(dataFolders) {
+        return Promise.all(dataFolders.map(folder =>
+            fs.readdir(`${this.dirs.sourceTemplates}/${folder}/${this.dirs.dataPath}`)
+                .then(dataFiles => this.processDataFiles(folder, dataFiles))
+                .then(jsonMap => this.saveJSON(folder, jsonMap))
+        ))
+    }
+
+    processDataFiles(folder, dataFiles) {
+        return Promise.all(dataFiles.map(file =>
+            fs.readJSON(`${this.dirs.sourceTemplates}/${folder}/${this.dirs.dataPath}/${file}`)
+                .then(data => fs.exists(`${this.dirs.sourceTemplates}/${folder}/${this.dirs.imagePath}/${file}`)
+                    .then(exists => {
+                        if(exists){
+                            return fs.readJSON(`${this.dirs.sourceTemplates}/${folder}/${this.dirs.imagePath}/${file}`)
+                                .then(images => this.injectImagesIntoDataFile(images, data))
+                        }})
+                    .then(() => {
+                        const jsonKey = file.startsWith("_") ? file.substring(1, file.lastIndexOf(".")) : file.substring(0, file.lastIndexOf(".")) 
+                        return { [jsonKey]: data }
+                    })
+                )
+        ))
+    }
+    
+    processRawItem(files, alternates){
+        return Promise.all(files.map(({ src, size, dest }) => {
+            const srcName = src.slice(src.lastIndexOf("/") + 1, src.lastIndexOf("."))
+            const source = {
+                size,
+                name: (dest && dest.slice(dest.lastIndexOf("/") + 1).replace("[name]", srcName)) || srcName,
+                extension: src.slice(src.lastIndexOf("."))
+            }
+
+            return alternates ?
+                Promise.all(alternates.map(alter => this.savePicture(
+                    `${this.dirs.sourceImages}/${src}`,
+                    { src: this.generateFileName(source, alter.dest), size: alter.size, },
+                    source.extension
+                )))
+                : this.savePicture(
+                    `${this.dirs.sourceImages}/${src}`,
+                    { src: this.generateFileName(source, dest), size },
+                    source.extension
+                )
+        }))
+    }
+
+    injectImagesIntoDataFile(images, data){
+        return Promise.all(images.map(entry => entry.set ?
+            Promise.all(entry.set.map(async (item, index) =>
+                this.createPortionPictures(item).then(portion =>
+                    this.index(data, entry.path.replace("[]", index), portion))
+            )) :
+            this.createPortionPictures(entry).then(portion => 
+                this.index(data, entry.path, portion)
+            )
+        ))
+    }
+
+    createPortionPictures(entry) {
+        return Promise.all(entry.files.map((item, index) => {
+            const source = this.parseSource(entry.files.length, entry.alt, index, item)
+            const resultPortion = {}
+            return this.createPictureSources(source, entry.imageTemplate)
+                .then(pSources => resultPortion.sources = pSources)
+                .then(() => this.createPictureImg(source, entry.imageTemplate))
+                .then(pImg => resultPortion.img = pImg)
+                .then(() => this.createImg(source, item.dest))
+                .then(img => ({ ...img, ...resultPortion }))
+
+        })).then(files => files.length === 1 ? files[0] : files)
+    }
+
+    createPictureSources(source, { sources } = {}) {
+        if (sources) {
+            return Promise.all(sources.map(pictureSourcesPartial => {
+                const pictureSource = {
+                    media: pictureSourcesPartial.media,
+                    sizes: pictureSourcesPartial.sizes,
+                    srcset: pictureSourcesPartial.srcset.map(srcItem => ({
+                        file: srcItem.src,
+                        src: this.generateFileName(source, srcItem.dest),
+                        size: srcItem.size
+                    }))
+                }
+
+                return Promise.all(pictureSource.srcset.map(
+                    srcItem => {
+                        const file = srcItem.file; delete srcItem.file
+                        return this.savePicture(`${this.dirs.sourceImages}/${file}`, srcItem, source.extension)
+                    })).then(() => pictureSource)
+            }))
+        } else return Promise.resolve()
+    }
+
+    createPictureImg(source, { img } = {}) {
+        if (img) {
+            const pictureImg = {
+                src: source.src,
+                sizes: img.sizes,
+                alt: source.alt,
+                srcset: img.srcset.map(srcItem => ({
+                    src: this.generateFileName(source, srcItem.dest),
+                    size: srcItem.size
+                }))
+            }
+
+            return Promise.all(pictureImg.srcset.map(
+                srcItem => this.savePicture(`${this.dirs.sourceImages}/${source.src}`, srcItem, source.extension))
+            ).then(() => pictureImg)
+        } else return Promise.resolve()
+    }
+
+    createImg(source, dest) {
+        const img = {
+            src: this.generateFileName(source, dest),
+            size: source.size,
+            alt: source.alt
+        }
+
+        return this.savePicture(`${this.dirs.sourceImages}/${source.src}`, img, source.extension)
+            .then(() => img)
+    }
+
+    parseSource(filesLength, alt, index, item) {
+        const srcName = item.src.slice(item.src.lastIndexOf("/") + 1, item.src.lastIndexOf("."))
+        const entryIndex = filesLength > 1 ? index + 1 : 0
+        return {
+            index: entryIndex,
+            alt: alt && entryIndex ? alt + " " + entryIndex : alt,
+            name: (item.dest && item.dest.slice(item.dest.lastIndexOf("/") + 1).replace("[name]", srcName)) || srcName,
+            extension: item.src.slice(item.src.lastIndexOf(".")),
+            src: item.src,
+            size: item.size
+        }
+    }
+
+    stripInvalid(str){
+        return str && typeof str === "string" ? str.replace(/[|&$%@"<>()+,]/g, "") : undefined
+    }
+
+    generateFileName({ name, index, size, extension } = {}, dest) {
+        let filename = this.stripInvalid(dest ?
+            dest
+                .replace("[name]", name)
+                .replace("[index]", index ? index : 1)
+                .replace("[size]", size ? size : "")
+            : name)
+
+        if (!filename) {
+            throw new Error("Invalid destination name.")
+        }
+
+        return `${this.dirs.outputFolder}/${this.dirs.imagePath}/${filename}${extension}`
+    }
+
+    index(obj, objPath, value) {
+        if (typeof objPath == "string")
+            return this.index(obj, objPath.split("."), value)
+        else if (objPath.length == 1 && value !== undefined)
+            return obj[objPath[0]] = value
+        else if (objPath.length == 0)
+            return obj
+        else return this.index(
+            obj[(isNaN(objPath[0]) ? objPath[0] : parseInt(objPath[0]))],
+            objPath.slice(1),
+            value)
+    }
+
+    getDependencies(dir, compilationDependenciesSet, rootdir, dependencies) {
+        const list = fs.readdirSync(dir)
+        list.forEach(file => {
+            file = dir + "/" + file
+            const stat = fs.statSync(file)
+            if (stat && stat.isDirectory()) {
+                this.getDependencies(file, compilationDependenciesSet, rootdir, dependencies)
+            } else {
+                if (file.slice(file.lastIndexOf(".")) === ".json") {
+                    dependencies.push(file)
+                    compilationDependenciesSet.add(rootdir + "\\" + file.replace(this.slashRegex, "\\"))
+                }
+            }
+        })
+    }
+
+    getChangedDependencies(compilation) {
+        const folders = {}
+        const files = {}
+        const changedFolders = new Set()
+        const changedPureFiles = []
+        const fileDependencies = []
+
+        compilation.contextDependencies.add(compilation.compiler.context + "\\" + this.dirs.sourceTemplates.replace(this.slashRegex, "\\"))
+        this.getDependencies(this.dirs.sourceTemplates, compilation.fileDependencies, compilation.compiler.context, fileDependencies)
+
+        fileDependencies.forEach(rawFileName => {
+            const folderFile = rawFileName.slice(rawFileName.indexOf(this.dirs.sourceTemplates) + this.dirs.sourceTemplates.length + 1, rawFileName.lastIndexOf("/"))
+            const folder = folderFile.slice(0, folderFile.indexOf("/"))
+            const group = folderFile.slice(folderFile.indexOf("/") + 1)
+
+            const time = fs.statSync(rawFileName).mtime.getTime()
+            if ((group === this.dirs.dataPath || group === this.dirs.imagePath) && folder){
+                folders[folder] = folders[folder] ? folders[folder] : {
+                    lastUpdate: [],
+                    filenames: []
+                }
+                
+                folders[folder].lastUpdate.push(time)
+                folders[folder].filenames.push(rawFileName.slice(rawFileName.lastIndexOf(group)))
+            } else {
+                if (this.files[rawFileName] !== time) {
+                    changedPureFiles.push(rawFileName)
+                }
+                files[rawFileName] = time 
+            }
+        })
+
+        for (let folder in folders){
+            folders[folder].lastUpdate = folders[folder].lastUpdate.sort().reverse()[0]
+            if(!this.folders[folder] || 
+                this.folders[folder].lastUpdate < folders[folder].lastUpdate ||
+                this.folders[folder].filenames.length != folders[folder].filenames.length
+            ){
+                changedFolders.add(folder)
+            }
+        }
+
+        return {
+            folders,
+            files,
+            changedFolders: Array.from(changedFolders),
+            changedPureFiles
+        }
+    }
+}
